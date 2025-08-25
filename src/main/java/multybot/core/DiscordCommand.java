2e2bf42f5e302a2814d7d302b791e759c0aafb2b
capi <luxorcapi@gmail.com>
2025-08-26 01:27:31 +0200

    DiscordCommand.java Command logic added

---
+++ src/main/java/multybot/core/DiscordCommand.java
@@ -0,0 +1,10 @@
+package dev.lcapi.multybot.core;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DiscordCommand {
+    String name();
+    String descriptionKey(); // clave i18n (p.e. "ping.description")
+}
