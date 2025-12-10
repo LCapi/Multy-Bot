6daa9e9a9cc999281eca606bf2b92d3003eec771
capi <luxorcapi@gmail.com>
2025-12-10 23:40:28 +0100

    DiscordCommand flag implemented

--- src/main/java/multybot/core/DiscordCommand.java
+++ src/main/java/multybot/core/DiscordCommand.java
@@ -1,10 +1,28 @@
 package multybot.core;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(TYPE)
 public @interface DiscordCommand {
+
+    /**
+     * Slash command name (ej: "ping", "ban", "poll"...)
+     */
     String name();
-    String descriptionKey(); // clave i18n (p.e. "ping.description")
+
+    /**
+     * Key en el bundle de i18n para la descripción base.
+     */
+    String descriptionKey();
+
+    /**
+     * Allow to activate/disable comands.
+     * if false the command won't be available.
+     */
+    boolean enabled() default true;
 }
