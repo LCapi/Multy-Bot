446e6f1ad0fa85b50a7d17508606eddabc6e2b1a
capi <luxorcapi@gmail.com>
2026-03-12 21:48:13 +0100

    Alimentar ese estado desde el gateway y JDA

--- src/main/java/multybot/DiscordGateway.java
+++ src/main/java/multybot/DiscordGateway.java
@@ -6,6 +6,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import multybot.core.ListenerRegistry;
+import multybot.infra.DiscordGatewayState;
 import net.dv8tion.jda.api.JDA;
 import org.jboss.logging.Logger;
 
@@ -16,11 +17,13 @@ public class DiscordGateway {
 
     @Inject JDA jda;
     @Inject ListenerRegistry listenerRegistry;
+    @Inject DiscordGatewayState gatewayState;
 
     void onStart(@Observes StartupEvent ev) {
         LOG.info("Starting DiscordGateway...");
         listenerRegistry.registerAll(jda);
         LOG.info("DiscordGateway started (listeners wired).");
+        gatewayState.markStarted();
     }
 
     void onStop(@Observes ShutdownEvent ev) {
